24649108c68d3d912d0c70efa090a0d6251df42c
GeoIsrael <MDf4nBNjqrufGnxS@protonmail.com>
2021-06-18 10:18:21 +0300

    streams

--- src/main/java/com/geo/phones/Calculator.java
+++ src/main/java/com/geo/phones/Calculator.java
@@ -6,8 +6,11 @@ public class Calculator {
 
     public static void main(String[] args) {
 
-        String line = "8957645537849";
+        String line = "895764553784";
         System.out.println(Arrays.stream(line.split("")).map(i -> Integer.parseInt(i)).reduce(0,(a,b) -> a+b));
+        System.out.println(Arrays.stream(line.split("")).map(i -> Integer.parseInt(i)).reduce(0,Integer::sum));
+        System.out.println(Arrays.stream(line.split("")).reduce("",String::concat));
+        System.out.println(Arrays.stream(line.split("")).reduce("",String::join));
 
     }
 }
