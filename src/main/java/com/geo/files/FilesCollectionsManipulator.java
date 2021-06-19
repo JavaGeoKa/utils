c07ae73f72ee8ecda2b61237bf5dd00b0e542f47
GeoIsrael <MDf4nBNjqrufGnxS@protonmail.com>
2021-06-19 15:42:53 +0300

    files

---
+++ src/main/java/com/geo/files/FilesCollectionsManipulator.java
@@ -0,0 +1,17 @@
+package com.geo.files;
+
+public class FilesCollectionsManipulator {
+
+
+
+    public static void main(String[] args) {
+
+
+//        Files.lines(Paths.get(requestsOutFile)).forEach(requests::add);   //add lines to List<String>
+
+//        Files.write(Paths.get(requestsOutFile), (Iterable<String>)requests::iterator);  //save list to file
+
+    }
+
+
+}
